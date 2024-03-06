660bdba275de572ecb81ddce1492f43f943e5965
Vladimir <surorion@mail.ru>
2024-03-06 22:28:56 +0300

    startCommits

---
+++ src/main/java/ru/courseproject/project/config/BotConfig.java
@@ -0,0 +1,24 @@
+package ru.courseproject.project.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+@Configuration
+@PropertySource("application.properties")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BotConfig {
+
+    @Value("${bot.name}")
+    private String botName;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+}
